e92bec3c40507c13950600bd8081fcc8b211c8cb
Damastier <example@mail.ru>
2022-10-09 20:25:41 +0300

    Create modules download and showinfo

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
 import Difference from './modules/difference';
 import Form from './modules/forms';
+import ShowInfo from './modules/showinfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
   const mainSlider = new MainSlider({
@@ -47,7 +49,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
   new VideoPlayer('.showup .play', '.overlay').init();
   new VideoPlayer('.module__video-item .play', '.overlay').init();
+
   new Difference('.officerold', '.officernew', '.officer__card-item').init();
   new Form('.form').init();
+
+  new ShowInfo('.plus__content').init();
+
+  new Download('.download').init();
 
 });
---
+++ src/js/modules/download.js
@@ -0,0 +1,29 @@
+export default class Download {
+  constructor(treeggers) {
+    this.btns = document.querySelectorAll(treeggers);
+    this.path = 'assets/img/mainbg.jpg';
+  }
+
+  downloadItem(path) {
+    const element = document.createElement('a');
+    element.setAttribute('href', path);
+    element.setAttribute('download', 'item');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
+  init() {
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        this.downloadItem(this.path);
+        e.preventDefault()
+      });
+    });
+  }
+
+}
